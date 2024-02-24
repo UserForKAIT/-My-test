515a42a089fcf2f12e39536883ad291aa3fc4873
Coder <161161399+UserForKAIT@users.noreply.github.com>
2024-02-25 01:41:41 +0300

    code
    Tests in different programming languages.

---
+++ test.js
@@ -0,0 +1,7 @@
+let year = prompt('connect?', '');
+
+if (year == 1) {
+  alert( 'yes' );
+} else {
+  alert( 'not' );
+}
---
+++ test.py
@@ -0,0 +1,7 @@
+def test(answer):
+    if answer == 1:
+        return True
+    else:
+        return False
+
+test(0) # 1 or ...
